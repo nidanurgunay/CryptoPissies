c01c4504e581a14146232f373c44e5fae70c28c3
furkan-celik <game.wive@gmail.com>
2021-05-13 23:14:58 +0300

    initial project

---
+++ src/contracts/Color.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.5.0;
+
+import "./ERC721Full.sol";
+
+contract Color is ERC721Full {
+    string[] public colors;
+    mapping(string => bool) _colorExists;
+
+    constructor() ERC721Full("Color", "COLOR") public {
+
+    }
+
+    function mint(string memory _color) public {
+        // Require unique color
+        require(!_colorExists[_color]);
+        // Color - add it
+        uint _id = colors.push(_color);
+        // Call the mint function
+        _mint(msg.sender, _id);
+        // Color - track it
+        _colorExists[_color] = true;
+    }
+}
